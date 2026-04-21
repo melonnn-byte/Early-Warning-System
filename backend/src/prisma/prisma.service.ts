847246b2d95a190f1b52cf99f4567333c3c30ccf
wirashauma <wirashaumaa@gmail.com>
2026-04-21 16:14:29 +0700

    feat: implement PrismaService for database interaction

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleDestroy {
+  constructor() {
+    const connectionString =
+      process.env.DATABASE_URL ??
+      'postgresql://postgres:postgres@localhost:5432/postgres';
+
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+
+    super({
+      adapter,
+      log: ['warn', 'error'],
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
