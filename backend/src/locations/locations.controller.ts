11666f70b09c8492647412dc32df91b78dc16ab1
wirashauma <wirashaumaa@contoh.com>
2026-04-23 17:21:38 +0700

    feat: add LocationsController for retrieving sensor locations

---
+++ backend/src/locations/locations.controller.ts
@@ -0,0 +1,17 @@
+import {
+  Controller,
+  Get,
+} from '@nestjs/common';
+import { ok } from '../common/api-response';
+import { LocationsService } from './locations.service';
+
+@Controller('locations')
+export class LocationsController {
+  constructor(private readonly locationsService: LocationsService) {}
+
+  @Get('sensors')
+  async getSensorLocations() {
+    const data = await this.locationsService.getSensorLocations();
+    return ok(data);
+  }
+}
